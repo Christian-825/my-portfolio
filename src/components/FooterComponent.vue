85fb8b26edd0606914bbad71d9390424178770bc
chan12nievera <chan12nievera@gmail.com>
2025-07-29 12:09:08 +0800

    Added web portfolio source

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,9 @@
+<template>
+	<!-- Footer -->
+	<footer class="text-white text-center py-5">
+	  <p class="text-white ">Christian Nievera</p>
+	  <p class="text-white ">Full Stack Web Developer</p>
+	  <p class="text-white ">&copy; All Rights Reserved 2025</p>
+	</footer>
+	<!-- Footer End -->
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+    <a class="navbar-brand brand-text pl-5 py-3" href="#landing">TyokScript</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse pl-4 pr-4 my-1" id="navbarNav">
+      <ul class="navbar-nav ml-auto pl-5 text-start text-lg-left">
+        <li class="nav-item"><a class="nav-link link-text" href="#landing">Home</a></li>
+        <li class="nav-item"><a class="nav-link link-text" href="#projects">My Projects</a></li>
+        <li class="nav-item"><a class="nav-link link-text" href="#tools">Tools</a></li>
+        <li class="nav-item"><a class="nav-link link-text" href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+
+<script setup>
+// No JavaScript 
+</script>
+
+
