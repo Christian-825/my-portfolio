17322e5fa2422c6e0ca01c8ba662b43443bff408
chan12nievera <chan12nievera@gmail.com>
2025-08-28 22:56:02 +0800

    Update about desc

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
      <!-- Profile Start -->
       <div id="landing" class="row align-items-center justify-content-center px-3 py-5 py-lg-5 g-4">
         <div class="col-10 col-md-3 py-lg-5">
-          <img src="/images/me.jpg" class="rounded-5 img-fluid mb-5 myProfile" alt="Christian Nievera">
+          <img src="/images/me.jpg" class="rounded-circle img-fluid mb-5 myProfile" alt="Christian Nievera">
         </div>
 
         <div class="col-md-7 col-lg-5 text-center text-md-start pl-md-5 py-lg-5 mt-3">
@@ -10,8 +10,7 @@
           <h1 class="fw-bold text-md-left" id="lastName">Nievera</h1>
           <h3 class="text-md-left" id="myRole">Full Stack Web Developer</h3>
           <p class="text-md-left" id="myDesc">
-            Currently enrolled in a web development bootcamp where I’m learning to build modern, responsive websites using HTML, CSS, JavaScript, and popular frameworks like Bootstrap and React. I’m passionate about creating clean, user-friendly interfaces and continuously improving my coding skills through real-world projects and hands-on experience.
-          </p>
+            Bootcamp-trained web developer passionate about creating clean, responsive, and intuitive websites. Experienced in taking projects from concept to completion, designing interfaces, implementing functionality, and delivering seamless user experiences. Always learning new technologies and tackling real-world challenges to build meaningful digital solutions.          </p>
         </div>
       </div>
       <!-- Profile End -->
